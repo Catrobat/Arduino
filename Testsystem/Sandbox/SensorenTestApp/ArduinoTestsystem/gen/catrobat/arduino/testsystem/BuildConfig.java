3f19952166da938ad26d9699366a798c3aa98467
Marco Steger <marco.st1987@gmail.com>
2013-10-09 12:59:53 +0200

    rm project test files

--- Testsystem/Sandbox/SensorenTestApp/ArduinoTestsystem/gen/catrobat/arduino/testsystem/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package catrobat.arduino.testsystem;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Testsystem/Sandbox/SensorenTestApp/ArduinoTestsystemTests/gen/catrobat/arduino/testsystem/test/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package catrobat.arduino.testsystem.test;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
